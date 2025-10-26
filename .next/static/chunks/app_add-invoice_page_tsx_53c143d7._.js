76f86a14b5c2295561265f0b983a1d6b4bdc6310
vahid nasirzad <vahidnasirzad@icloud.com>
2025-10-27 02:37:34 +0330

    deleted button

--- .next/static/chunks/app_add-invoice_page_tsx_53c143d7._.js
+++ .next/static/chunks/app_add-invoice_page_tsx_53c143d7._.js
@@ -393,7 +393,7 @@ function AddInvoice() {
                                     }, this),
                                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("option", {
                                         value: "General",
-                                        children: "مشترک"
+                                        children: "General"
                                     }, void 0, false, {
                                         fileName: "[project]/app/add-invoice/page.tsx",
                                         lineNumber: 166,
